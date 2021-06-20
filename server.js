ed938105dc4cd407fc72c56f3296cd808773c0ec
dfealves <daniloferreira.alves@outlook.com>
2021-06-20 11:50:36 -0300

    fix package.json

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.static(__dirname + "/dist"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile("index.html", { root: "dist" });
 });
 
 // Inicia a aplicação pela porta configurada
